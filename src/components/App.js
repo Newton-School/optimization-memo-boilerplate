fc9bc1c39bc71119cbd616ff247f9071ff4106d9
Satyakiran Patnala <75940037+galaxey56@users.noreply.github.com>
2022-11-04 18:50:29 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,14 @@ import { useState, useMemo } from "react";
 import "../styles/App.css";
 
 function expensiveOperation(num) {
-  
+  console.log("expensiveOperation called");
+  let i = 0;
+  const result = Array(num).fill(0);
+  while (i < 1000000000) {
+    result[i % num] += i;
+    i++;
+  }
+  return result;
 }
 
 const App = () => {
