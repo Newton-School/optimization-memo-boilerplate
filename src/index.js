428c1bd2b634d051bd55ac417a14e4f3a86713d9
galaxey56 <satyakiran56@gmail.com>
2022-11-07 19:30:03 +0530

    added function

--- src/index.js
+++ src/index.js
@@ -2,6 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
+const Index = () => {
+  const [inc, setInc] = React.useState(0);
+  const onClick = () => setInc(inc + 1);
+  const renderCallback = (id, name, actualDuration) => {
+    console.log(actualDuration);
+    localStorage.setItem("timeTaken", actualDuration);
+  };
+  return (
+    <div id="main">
+      <React.Profiler id="Optimization" onRender={renderCallback}>
+        <App />
+        <button id="render" onClick={onClick}>
+          Re-render
+        </button>
+      </React.Profiler>
+    </div>
+  );
+};
 
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Index />, document.getElementById("root"));
